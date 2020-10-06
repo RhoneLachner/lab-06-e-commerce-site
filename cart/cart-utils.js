e2255eee3649ab2178af88706cf05fc2ef74b645
Rhone Lachner <fernandclay@gmail.com>
2020-10-06 14:59:23 -0700

    added cart html file with outline

---
+++ cart/cart-utils.js
@@ -0,0 +1,9 @@
+import { books as sourceOfTruth } from '../data.js';
+import { findById } from '../utils.js';
+
+/*
+    const cartItem = {
+        id: 'airplanes',
+        quantity: 2
+    };
+*/
